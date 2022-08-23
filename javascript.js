ec5ef8dc30916c8d40b1dca27022cb515ffd248d
Sebastian <sjflores2@espe.edu.ec>
2022-08-23 16:41:11 -0500

    Servicios (Codigo js)

---
+++ javascript.js
@@ -0,0 +1,21 @@
+function taxi(){
+    let guardar1=document.getElementById("txt1").value;
+    let guardar2=document.getElementById("txt2").value;
+    let guardar3=document.getElementById("txt3").value;
+    if(guardar1==""){
+    alert("Cuadro de texto 1 vacio - llenar por favor")
+    }
+    if(guardar2==""){
+        alert("Cuadro de texto 2 vacio - llenar por favor")
+
+    }
+    if(guardar3==""){
+
+        alert("Cuadro de texto 3 vacio - llenar por favor")
+
+    }else{
+        alert(guardar1+ " Ubicacion " +guardar2+ " con destino a "+guardar3+ " -Servicio Contratado-");
+
+    }
+
+}
