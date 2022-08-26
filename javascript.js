6a225ecf28eeda6afe7a3d9278840d3424210f2d
Jean Guanin <jpguanin@espe.edu.ec>
2022-08-25 20:02:42 -0500

    Cambios Importantes

--- javascript.js
+++ javascript.js
@@ -94,3 +94,22 @@ function cancelar(){
     document.getElementById("texto3").innerHTML="";
     document.getElementById("texto4").innerHTML="";
 }
+
+
+$('.sd').click(function(){
+	$('.hero, .content').addClass('scrolled');
+});
+
+$('.hero').mousewheel(function(e){
+	if( e.deltaY < 0 ){
+		$('.hero, .content').addClass('scrolled');
+		return false;
+	}
+});
+$(window).mousewheel(function(e){
+	if( $('.hero.scrolled').length ){
+		if( $(window).scrollTop() == 0 && e.deltaY > 0 ){
+			$('.hero, .content').removeClass('scrolled');
+		}
+	}
+});
